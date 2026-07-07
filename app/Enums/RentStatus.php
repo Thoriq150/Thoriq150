02a092ee4a41145c9a185abe950a7e4a752fa097
Thoriq Azhar Fauzan <115950790+Thoriq150@users.noreply.github.com>
2026-07-07 11:09:13 +0700

    Delete app directory

--- app/Enums/RentStatus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum RentStatus : string {
-    case Out  = 'Kendaraan Sedang Digunakan';
-    case In   = 'Kendaraan Berhasil Dikembalikan';
-}
--- app/Enums/VehicleStatus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum VehicleStatus : string {
-    case Active = 'Tersedia';
-    case Out    = 'Kendaraan Sedang Digunakan';
-}
